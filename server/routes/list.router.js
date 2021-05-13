7d93f942ac7af6a89884b1f7f613a3dcf4b3e714
Steve Burket <burketsteve@gmail.com>
2021-05-13 12:24:02 -0500

    PUTreset router working

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,19 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+// PUT reset
+
+router.put('/reset', (req, res) => {
+  let queryText = `UPDATE "list" SET "purchase_status"=false RETURNING *`;
+  pool.query(queryText)
+  .then(result => {
+    res.send(result.rows);
+  })
+  .catch((err) => {
+    console.log(`Error making query: ${queryText}`, err);
+    
+  })
+
+})
 
 module.exports = router;
