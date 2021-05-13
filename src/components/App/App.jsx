ecd9dee0e1e1890e641cc3b0e5d504663eca8df0
Steve Burket <burketsteve@gmail.com>
2021-05-13 17:11:17 -0500

    installed axios, added a note, updated ItemForm component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
 import { response } from 'express';
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from '../Header/Header.jsx';
---
+++ src/components/ItemForm/ItemForm.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+function ItemForm(props) {
+  console.log(`ItemForm Props are: `, props);
+
+  const [itemName, setItemName] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [unit, setUnit] = useState(0);
+
+  function onSubmit(event) {
+    event.preventDefault();
+    console.log(itemName, quantity, unit);
+
+    const newItem = {
+      name: itemName,
+      quantity: quantity,
+      unit: unit
+    };
+
+    props.addItem(newItem);
+
+    console.log(newItem);
+
+    // clear the form
+    setItemName('');
+    setQuantity('');
+    setUnit('');
+  }
+
+  return (
+    <div class='input-form'>
+      <h2>Add an Item</h2>
+      <form onSubmit={onSubmit}>
+        <label for='item'>Item: </label>
+        <input
+          id='item'
+          placeholder='item'
+          value={itemName}
+          onChange={(event) => setItemName(event.target.value)}
+          required
+        />
+        <br />
+
+        <label for='quantity'>Quantity: </label>
+        <input
+          id='quantity'
+          placeholder='quantity'
+          onChange={(event) => setQuantity(event.target.value)}
+          value={quantity}
+          required
+        />
+        <label for='unit'>Unit: </label>
+        <input
+          id='unit'
+          placeholder='unit'
+          value={unit}
+          onChange={(event) => setUnit(event.target.value)}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default ItemForm;
