5758feb9aeb248f7d3d1d55f55bd1f93120e7df3
Matthew Sharp <msharp633@gmail.com>
2021-05-13 12:15:28 -0500

    this was making both delete all from the list and delete jsut from the id

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,33 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+router.delete('/:id', (req, res) => {
+    console.log(req.params);
+    const resetId = req.params.id;
+
+    let queryText = `DELETE from "list" WHERE id=$1`;
+    pool.query(queryText, [resetId])
+        .then(() => {
+            res.sendStatus(202);
+        }).catch((err) => {
+            console.log(`Error making query ${queryText}`)
+            res.sendStatus(500);
+        });
+});
+
+router.delete('/', (req, res) => {
+    console.log(req.params);
+    const clearId = req.params.id;
+
+    let queryText = `DELETE from "list"`;
+    pool.query(queryText)
+        .then(() => {
+            res.sendStatus(202);
+        }).catch((err) => {
+            console.log(`Error making query ${queryText}`)
+            res.sendStatus(500);
+        });
+});
+
 
 module.exports = router;
