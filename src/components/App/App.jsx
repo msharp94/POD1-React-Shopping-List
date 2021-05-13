f3e61b619d6cc251e70358aeda23643596e404fc
Steve Burket <burketsteve@gmail.com>
2021-05-13 15:04:14 -0500

    axios set for POST and PUT-reset

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
+import { useState, useEffect} from 'react';
+import axios from 'axios';
 
-import Header from '../Header/Header.jsx'
+import Header from '../Header/Header.jsx';
 import './App.css';
 
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
-        </div>
-    );
+
+    const [newItem, setNewItem] = useState([]);
+
+    useEffect(() => {
+        console.log('App is loaded!');
+        displayList();
+    }, []);
+
+    function addNewItem(newItem) {
+      /*{create object in item form compnent} */
+
+      axios({
+        method: 'POST',
+        url: '/lists',
+        data: newItem /*{create object newItem in item form compnent} */
+      })
+        .then((response) => {
+          displayList();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    function resetItems() {
+        axios({
+            method: 'PUT',
+            url: '/list',
+            data: /*?*/
+        }).then((response) => {
+            if 
+        })
+    }
+
+
+  return (
+    <div className='App'>
+      <Header />
+      <main>
+        <p>Under Construction...</p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
